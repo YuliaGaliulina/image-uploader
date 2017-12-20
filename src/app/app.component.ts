d5821dccb9c90d348a92d202b86a562326b344e3
julia galiulina <julie_gal@mail.ru>
2017-12-20 15:49:05 +0500

    inject the service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { UploaderService } from "./uploader.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+  constructor(private uploader: UploaderService) {}
+
+  progress: { percentage: number } = { percentage: 0 };
+  fileUrl: { url: string } = { url: "" };
+
+  upload(file) {
+    this.uploader.uploadFile(file, this.progress, this.fileUrl);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AngularFireModule } from "angularfire2";
+import { AngularFireDatabase } from "angularfire2/database";
+import * as firebase from "firebase";
+import { UploaderService } from "./uploader.service";
+import { AppComponent } from "./app.component";
 
-
-import { AppComponent } from './app.component';
-
+export const firebaseConfig = {
+  apiKey: "AIzaSyBylAkzXh1mhGrDJdxLmGsECTxC4dRlzpQ",
+  authDomain: "image-uploader-7d6c8.firebaseapp.com",
+  databaseURL: "https://image-uploader-7d6c8.firebaseio.com",
+  projectId: "image-uploader-7d6c8",
+  storageBucket: "image-uploader-7d6c8.appspot.com",
+  messagingSenderId: "836200457783"
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AngularFireModule.initializeApp(firebaseConfig)],
+  providers: [AngularFireModule, UploaderService, AngularFireDatabase],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
