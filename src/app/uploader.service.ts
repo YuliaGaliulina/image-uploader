c969c6d064c51917773d5f7bcdccc855d10a9670
julia galiulina <julie_gal@mail.ru>
2017-12-20 15:46:09 +0500

    uploader service

---
+++ src/app/uploader.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@angular/core";
+import * as firebase from "firebase";
+import { AngularFireDatabase } from "angularfire2/database";
+
+@Injectable()
+export class UploaderService {
+  constructor(private db: AngularFireDatabase) {}
+
+  basePath: string = "uploads/";
+  imageUrl: string;
+
+  uploadFile(file, progress: { percentage: number }, fileUrl: { url: string }) {
+    const storageRef = firebase.storage().ref(this.basePath + file.name);
+    const uploadTask = storageRef.put(file);
+
+    uploadTask.on(
+      firebase.storage.TaskEvent.STATE_CHANGED,
+      snapshot => {
+        progress.percentage = Math.round(
+          snapshot.bytesTransferred / snapshot.totalBytes * 100
+        );
+      },
+      error => {
+        console.log(error);
+      },
+      () => {
+        fileUrl.url = uploadTask.snapshot.downloadURL;
+      }
+    );
+  }
+}
